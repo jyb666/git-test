5d9ae9f3d9dda3744423b00bd5c341fa60814eeb
jyb <jyb@123.com>
2022-05-15 21:52:44 +0800

    push commit

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("master test1");
         System.out.println("hot fix test!");
         System.out.println("hot fix test1!");
+        System.out.println("push test!");
     }
 }
